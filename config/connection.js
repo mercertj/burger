2759e6f09b447c8257a764221dbcc188be3e904f
Thomas Mercer <tmercer74@gmail.com>
2017-07-28 11:41:09 -0700

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -19,20 +19,24 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Fluffy#1",
-  database: "burger_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JASWDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Fluffy#1",
+        database: "burger_db"
+    });
+};
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
