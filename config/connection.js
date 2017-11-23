697ea358b9d54de1c010ee15ba476e1de317f4f2
Thomas Mercer <tmercer74@gmail.com>
2017-11-23 00:30:54 -0800

    working on localhost issue

--- config/connection.js
+++ config/connection.js
@@ -1,42 +1,42 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var methodOverride = require("method-override");
 
-var app = express();
-var port = 3000;
+// var app = express();
+// var port = 3000;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// // Serve static content for the app from the "public" directory in the application directory.
+// app.use(express.static("public"));
 
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// // Parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
-var exphbs = require("express-handlebars");
+// // Override with POST having ?_method=DELETE
+// app.use(methodOverride("_method"));
+// var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
-var connection;
+// var mysql = require("mysql");
+// var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "Fluffy#1",
-        database: "burgers_db"
-    });
-};
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: "localhost",
+//         user: "root",
+//         password: "Fluffy#1",
+//         database: "burgers_db"
+//     });
+// };
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
 
-module.exports = connection;
+// module.exports = connection;
--- server.js
+++ server.js
@@ -1,39 +1,25 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
 // Sets up the Express App
 // =============================================================
 var app = express();
-var path = require("path");
-var PORT = process.env.PORT || 3000 ;
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-    res.send('burger');
-})
-// parse application
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
-app.use(bodyParser.json())
-
-// app.listen(PORT, function() {
-//     console.log("App listening on PORT: " + PORT);
-// });
-
-app.use(express.static(process.cwd() + '/public'));
-
-app.use(methodOverride('_method'))
-var exphbs = require('express-handlebars');
-app.engine('hbs', exphbs({
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
+app.set('view engine', 'handlebars');
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-var routes = require('./controllers/burgers_controller.js');
+var routes = require('./controllers/routes.js');
 app.use('/', routes);
 
-app.listen(PORT, function(){
-	console.log("App listening on PORT: " + PORT);
-
-});
+var port = 3000;
+console.log("listening on PORT 3000");
+app.listen(port);
